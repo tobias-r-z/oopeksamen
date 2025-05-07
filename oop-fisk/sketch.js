5cec19b7c02709971b086bd2dfb67383c6dd050a
tobias-r-z <tobiasrisager5@gmail.com>
2025-05-07 11:04:24 +0200

    fisk kan bevæge sig

--- oop-fisk/sketch.js
+++ oop-fisk/sketch.js
@@ -1,19 +1,35 @@
+let bvektor
+let velvektor
+let bfisk
+
 function setup() {
   createCanvas(400, 400);
+  bvektor = new Vektor(150,200)
+  velvektor = new Vektor(1,1)
+  bfisk = new fisk(bvektor,velvektor,60,30);
 }
 
 function draw() {
   background(220);
+  bfisk.show("orange","white");
+  bfisk.update()
 }
 
+
 class fisk {
-//parameter har en vektor som position og to size parameter for længde og bredde.
-  constructor(pos,sizeX,sizeY){
+//parameter har en vektor som position og to size parameter for længde og bredde, samt en hastighedsvektor.
+  constructor(pos,vel,sizeX,sizeY){
     this.pos=pos
     this.sizeX=sizeX
     this.sizeY=sizeY
+    this.vel=vel
   }
-  //funktion som tegner selve fisken
+  //funktion som skubber fisken, med en bestemt fart pr. frame.
+  update(){
+    this.pos=this.pos.add(this.vel)
+    this.boundaryCheck()
+}
+  //funktion som tegner selve fisken.
   show(col1,col2){
     fill(col1)
         triangle(this.pos.x, this.pos.y, 
@@ -25,8 +41,17 @@ class fisk {
         line(this.pos.x,this.pos.y,this.pos.x+this.sizeX/2,this.pos.y)
         circle(this.pos.x+this.sizeX/4,this.pos.y-this.sizeY/4,3)
   }
-  
+  boundaryCheck(){
+    // check that the fish is inside the canvas
+    if ((this.pos.x > width) || (this.pos.x < 0)) {
+        this.vel.x = this.vel.x * -1;
+    }
+    if ((this.pos.y > height) || (this.pos.y < 0)) {
+        this.vel.y = this.vel.y * -1;
+    }
 }
+}
+//en klasse som laver en vektor, til fisk og andre ting
 class Vektor {
   constructor(x,y){
     this.x=x
@@ -36,3 +61,9 @@ class Vektor {
     return new Vektor(this.x+other.x,this.y+other.y)
   }
 }
+
+
+
+
+
+
